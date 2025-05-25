54e84b5f63b2e59630701fc4ebb5ace4acc06f11
Kamil Sajdera <kamilsajdera@gmail.com>
2025-05-25 14:55:33 +0200

    Added fetching forecast from API function

---
+++ src/api/weatherApi.ts
@@ -0,0 +1,15 @@
+import { type ApiFetch } from "../types/weather";
+
+export async function fetchWeather(apiData: ApiFetch): Promise<object> {
+  const { latitude, longitude, tempUnit } = apiData;
+  const endpointURL = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=relativehumidity_2m,pressure_msl,visibility&daily=weathercode,precipitation_probability_max,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,rain_sum,snowfall_sum&current_weather=true&timezone=auto&temperature_unit=${tempUnit}`;
+
+  const response: Response = await fetch(endpointURL);
+
+  if (!response.ok) {
+    throw new Error("Error fetching data, try again.");
+  }
+
+  const data = await response.json();
+  return data;
+}
---
+++ src/types/weather.ts
@@ -0,0 +1,6 @@
+export type ApiFetch = {
+    latitude: number,
+    longitude: number,
+    tempUnit: string,
+}
+
