bac6ec7bd1f4c650e542fd89cca85bfd31f19c97
Kamil Sajdera <kamilsajdera@gmail.com>
2025-06-06 13:59:39 +0200

    Added HighlightCard component

---
+++ src/ui/highlight-card.ts
@@ -0,0 +1,36 @@
+import lottie from "lottie-web";
+
+function cartIcon(target: HTMLDivElement, iconName: string) {
+  lottie.loadAnimation({
+    container: target,
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: `./animations/${iconName}`,
+  });
+}
+
+export function createCard(
+  name: string,
+  content: string,
+  iconFile?: string,
+  iconWidth?: string
+): HTMLDivElement {
+  const card = document.createElement("div");
+  card.classList.add("current-weather-item");
+
+  const title = document.createElement("h4");
+  title.textContent = name;
+
+  card.appendChild(title);
+
+  card.innerHTML += content;
+
+  if (iconFile) cartIcon(card.querySelector(".card-icon")!, iconFile);
+
+  if(iconWidth) {
+    (card.querySelector(".card-icon")! as HTMLDivElement).style.width = `${iconWidth}%`
+  }
+
+  return card;
+}
