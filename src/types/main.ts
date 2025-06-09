a84cce11f6f2edd4adc98f16c838e9a1f92ba724
Kamil Sajdera <kamilsajdera@gmail.com>
2025-06-09 13:32:24 +0200

    Added object type for highlight cards

--- src/types/main.ts
+++ src/types/main.ts
@@ -2,3 +2,10 @@ export interface MatchedNames {
   mainName: string;
   captions: string[];
 }
+
+export type HighlightCard = {
+  name: string;
+  data: any;
+  iconFile?: string;
+  iconSize?: string;
+};
