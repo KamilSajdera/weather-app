f9beea40f7e3dd10c9607a53fc6d01f0944fad5f
Kamil Sajdera <kamilsajdera@gmail.com>
2025-06-09 14:04:11 +0200

    Renamed 'current_weather' => 'current' & added more types to it

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -7,13 +7,20 @@ export type ApiFetch = {
 export interface WeatherResponse {
   latitude: number;
   longitude: number;
-  current_weather: {
-    temperature: number;
-    weathercode: number,
+  current: {
+    temperature_2m: number;
+    weather_code: number,
+    wind_speed_10m: number,
+    rain: number,
+    relative_humidity_2m: number,
+    surface_pressure: number
   };
   daily: {
-    precipitation_probability_max: number[];
-    rain_sum: number[];
+    precipitation_probability_max: number[],
+    rain_sum: number[],
+    uv_index_max: number[],
+    sunrise: string[],
+    sunset: string[],
   };
   hourly: {
     temperature: number[];
