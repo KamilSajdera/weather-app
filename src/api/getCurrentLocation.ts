c77f5ad8911bea55a97b0304d636afc3d2ab40cf
Kamil Sajdera <kamilsajdera@gmail.com>
2025-06-17 00:12:14 +0200

    Added error info message

--- src/api/getCurrentLocation.ts
+++ src/api/getCurrentLocation.ts
@@ -1,11 +1,16 @@
 export async function getCurrentPosition(
   longitude: number,
   latitude: number
-): Promise<{ geonames: Array<{ name: string }> }> {
-  const response = await fetch(
-    `https://secure.geonames.org/findNearbyJSON?lat=${latitude}&lng=${longitude}&username=saydi`
-  );
-  const data = await response.json();
+) {
+  try {
+    const response = await fetch(
+      `https://secure.geonames.org/findNearbyJSON?lat=${latitude}&lng=${longitude}&username=saydi`
+    );
+    const data = await response.json();
 
-  return data;
+    return data;
+  } catch (error) {
+    console.warn(error);
+    return "We have problem with define name of your current location."
+  }
 }
